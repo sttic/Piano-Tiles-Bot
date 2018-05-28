5bbe3ba874e4644a9ca6e01b9412ae3d52cb21db
sttic <tommyd3689@gmail.com>
2018-05-28 10:34:44 -0400

    refactoring

--- piano-tiles.py
+++ piano-tiles.py
@@ -1,26 +1,22 @@
 from PIL import ImageGrab
 import pyautogui
-import time
 
-origin, bounds = (64, 0), (64, 0, 395, 590)
+bounds = (64, 0, 395, 590)
+origin, screen_width, screen_height = bounds[:2], bounds[2] - bounds[0], bounds[3] - bounds[1]
 tile_height = 150
-threshold, compensation = 32, 128
+threshold, sensor_offset, speed_compensation = 32, 25, 112
 
-screen = ImageGrab.grab(bounds)
-x = lambda i: origin[0] + screen.width/8 + i*screen.width/4
-# NOTE don't make num_sensors*step > 450
+x = lambda i: origin[0] + screen_width/8 + i*screen_width/4
 num_sensors, step = 4, 128
+assert num_sensors*step < screen_height
 
-sensors = [tuple((x(i), origin[1] + screen.height - 150 - step*s) for i in range(4)) for s in range(num_sensors)]
+sensors = [tuple((x(i), origin[1] + screen_height - tile_height - step*s) for i in range(4)) for s in range(num_sensors)]
 
 while True:
     screen = ImageGrab.grab(bounds)
 
     for sensor in sensors:
         for i in range(len(sensor)):
-            # subtract bit from sensors sample position to avoid irregular design of hold tiles
-            color = screen.getpixel((sensor[i][0] - origin[0] - 25, sensor[i][1] - origin[1]))
-
+            color = screen.getpixel((sensor[i][0] - origin[0] - sensor_offset, sensor[i][1] - origin[1]))
             if next((False for channel in color if channel > threshold), True):
-                # add distance to click to compensate for speed/delay
-                pyautogui.click(x=sensor[i][0], y=sensor[i][1] + compensation)
+                pyautogui.click(x=sensor[i][0], y=sensor[i][1] + speed_compensation)
