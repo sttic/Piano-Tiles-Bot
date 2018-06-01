b2645f3ad14b52e57dbb961a1f52675814c4c317
sttic <tommyd3689@gmail.com>
2018-05-31 23:30:53 -0400

    tweaking values

--- piano-tiles-bot.py
+++ piano-tiles-bot.py
@@ -10,17 +10,20 @@ class Screen:
     def read(self):
         def is_black(rgb):
             return next((False for channel in rgb if channel > threshold), True)
+            # return rgb == (0, 1, 0)
         height = self.screen.height
 
-        tiles = [[] for i in range(num_lanes)]
+        tiles = []
         for lane in range(len(sensors)):
             x = sensors[lane]
             # TODO handle if tile is at very bottom (unpausing or reviving)
-            for y in range(height - tile_height//2, 0, -1):
+            for y in range(height - speed_compensation, color_gap, -1):
                 color = self.screen.getpixel((x, y))
                 color_below = self.screen.getpixel((x, y + 1))
-                if is_black(color) and not is_black(color_below):
-                    tiles[lane].append(Tile(lane, y))
+                color_fill = self.screen.getpixel((x, y - color_gap))
+
+                if is_black(color) and not is_black(color_below) and is_black(color_fill):
+                    tiles.append(Tile(lane, y))
         return tiles
 
 class Tile:
@@ -31,27 +34,25 @@ class Tile:
         self.varient = varient
 
 def click(x, y):
-    pyautogui.click(x=x + origin[0], y=y + origin[1] + speed_compensation)
+    pyautogui.click(x=x + origin[0], y=y + origin[1] + speed_compensation, clicks=num_clicks)
 
 bounds = (64, 0, 395, 590)
 origin, screen_width, screen_height = bounds[:2], bounds[2] - bounds[0], bounds[3] - bounds[1]
 tile_height, num_lanes = 150, 4
-threshold, speed_compensation = 4, 90
+threshold, speed_compensation = 3, 52
+color_gap, num_clicks = 16, 3
 
-x = lambda i: screen_width/8 + i*screen_width/4
+x = lambda i: screen_width/(2*num_lanes) + i*screen_width/num_lanes
 sensors = [x(i) for i in range(num_lanes)]
-
-past_screen = Screen(bounds)
-past_tiles = past_screen.read()
-
-max_clicks = 1
 
 while True:
     current_screen = Screen(bounds)
     current_tiles = current_screen.read()
+    current_tiles.sort(key=lambda tile: tile.y, reverse=True)
+    count = 0
 
-    for lane in current_tiles:
-        for tile in lane:
-            click(tile.x, tile.y)
-
-# 1858
+    for tile in current_tiles:
+        click(tile.x, tile.y)
+        count += 1
+        if count >= 2:
+            break
