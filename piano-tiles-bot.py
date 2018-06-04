95837b03909290b78486badee4bcfea9287b2558
sttic <tommyd3689@gmail.com>
2018-06-04 19:40:26 -0400

    working version; no-delay clicks

--- piano-tiles-bot.py
+++ piano-tiles-bot.py
@@ -1,22 +1,19 @@
 from PIL import ImageGrab
 import pyautogui
-import time
 
 class Screen:
     def __init__(self, bounds):
         self.screen = ImageGrab.grab(bounds)
-        self.time = time.time()
 
     def read(self):
         def is_black(rgb):
             return next((False for channel in rgb if channel > threshold), True)
-            # return rgb == (0, 1, 0)
-        height = self.screen.height
 
+        height = self.screen.height
         tiles = []
+
         for lane in range(len(sensors)):
             x = sensors[lane]
-            # TODO handle if tile is at very bottom (unpausing or reviving)
             for y in range(height - color_gap, color_gap, -1):
                 color = self.screen.getpixel((x, y))
                 color_below = self.screen.getpixel((x, y + 1))
@@ -33,7 +30,7 @@ class Tile:
         self.y = y
         self.varient = varient
 
-def click(x, y, speed_compensation, offset=0):
+def click(x, y, speed_compensation=0, offset=0):
     x = x + origin[0]
     y = y + origin[1] + speed_compensation + offset
     if x > bounds[0] and x < bounds[2] and y > bounds[1] and y < bounds[3]:
@@ -48,27 +45,12 @@ color_gap, num_clicks = 16, 3
 x = lambda i: screen_width/(2*num_lanes) + i*screen_width/num_lanes
 sensors = [x(i) for i in range(num_lanes)]
 
-previous_time = 320
-beginning = time.time()
-speedometer = lambda t: 0.528846*t + 917
+pyautogui.PAUSE = 0
 
 while True:
-    start = time.time()
     current_screen = Screen(bounds)
     current_tiles = current_screen.read()
     current_tiles.sort(key=lambda tile: tile.y, reverse=True)
-    count = 0
 
     for tile in current_tiles:
-        end = time.time()
-        if time.time() - beginning < 0.2:
-            speed = 0
-            offset = 100
-        else:
-            speed = speedometer(end - beginning + previous_time)
-            offset = 32
-        speed_compensation = speed*(end - start)
-
-        click(tile.x, tile.y, speed_compensation, offset)
-
-    print(time.time() - beginning + previous_time)
+        click(tile.x, tile.y, offset=64)
